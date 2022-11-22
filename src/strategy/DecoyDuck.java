a8b61a891cb5a27e7edfe4cb57ba57f588914a3b
SLOWDOWNO <shi13341455279@gmail.com>
2022-11-22 14:46:50 +0800

    诱饵鸭继承Duck类

---
+++ src/strategy/DecoyDuck.java
@@ -0,0 +1,15 @@
+package strategy;
+
+public class DecoyDuck extends Duck {
+
+    public DecoyDuck() {
+        setFlyBehavior(new FlyNoWay());
+        setQuackBehavior(new MuteQuack());
+    }
+
+    @Override
+    public void display() {
+        System.out.print("I'm a duck Decoy");
+    }
+
+}
