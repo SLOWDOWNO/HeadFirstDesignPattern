ac0e87c8c4d5363a7d69ad101a175fc9b03ca673
SLOWDOWNO <shi13341455279@gmail.com>
2022-11-22 14:51:15 +0800

    橡皮鸭 继承Duck类

---
+++ src/strategy/RubberDuck.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class RubberDuck extends Duck{
+    public RubberDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Squeak();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a rubber duckie");
+    }
+}
