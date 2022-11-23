dc33e8403103f55b5da18fb368c5472d4cac6de7
SLOWDOWNO <shi13341455279@gmail.com>
2022-11-23 20:01:10 +0800

    此布告板跟踪最小,平均,最大的观测值,并显示

---
+++ src/observer/StatisticsDisplay.java
@@ -0,0 +1,40 @@
+package observer;
+//此布告板跟踪最小 平均 最大值 并显示
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum = 0.0f;
+    private int numReadings;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData)
+    {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure)
+    {
+        tempSum += temp;
+        numReadings++;
+
+        if (temp > maxTemp) {
+            maxTemp = temp;
+        }
+
+        if (temp < minTemp) {
+            minTemp = temp;
+        }
+
+        display();
+    }
+
+    @Override
+    public void display()
+    {
+        System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+                + "/" + maxTemp + "/" + minTemp);
+    }
+}
